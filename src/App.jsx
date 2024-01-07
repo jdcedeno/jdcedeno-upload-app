66c7ea65dc8792dc2a257d414b3ddc724d0e74c9
JDCedeno <josedcedenocode@gmail.com>
2024-01-06 21:13:53 -0500

    modal-01 create initial modal component with open/close functionality, need to add content and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import "./App.css"
+import React, { useState } from "react"
+import Modal from "./components/Modal"
 
 function App() {
-  return (
-    <>
-      <button>upload document</button>
-    </>
-  )
+	const [isModalOpen, setModalOpen] = useState(false)
+
+	const openModal = () => setModalOpen(true)
+	const closeModal = () => setModalOpen(false)
+
+	return (
+		<>
+			<button onClick={openModal}>upload document</button>
+
+			<Modal show={isModalOpen} onClose={closeModal}>
+				<p>this is the modal content</p>
+			</Modal>
+		</>
+	)
 }
 
 export default App
